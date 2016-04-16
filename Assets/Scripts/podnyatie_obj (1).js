11de93cfdfc0af4c4cc746b56d35292b46a3cc17
zevladnit <zevladnit@mail.ru>
2016-04-17 02:00:56 +0300

    Animation, physically object

--- Assets/Scripts/podnyatie_obj (1).js
+++ Assets/Scripts/podnyatie_obj (1).js
@@ -5,35 +5,60 @@ var RayDistance : float = 3.0;//дистанция
 private var Grab : boolean = false;//ф-ция притяжения
 private var Throw : boolean = false;//ф-ция толчка
 var offset : Transform;
+//private var open : boolean;
+//private var enter : boolean;
 
 
 function Update(){
-if (Input.GetKey(KeyCode.E)){//если нажата клавиша Е
+    if (Input.GetKey(KeyCode.E)){//если нажата клавиша Е
 
-Physics.Raycast(transform.position, transform.forward, hit, RayDistance);//физический луч
+        Physics.Raycast(transform.position, transform.forward, hit, RayDistance);//физический луч
 
-if(hit.rigidbody){//если луч соприкасается с rigidbody
-Grab = true;
-}
-}
+        if(hit.rigidbody){//если луч соприкасается с rigidbody
+            Grab = true;
+        }
+    }
 
-if (Input.GetMouseButtonDown(0)){//если нажата лев кн мыши
-if(Grab){
-Grab = false;
-Throw = true;
-}
-}
+    if (Input.GetMouseButtonDown(0)){//если нажата лев кн мыши
+        if(Grab){
+            Grab = false;
+            Throw = true;
+        }
+    }
 
-if(Grab){//ф-ция притяжения
-if(hit.rigidbody){
-hit.rigidbody.velocity = (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass))*grabPower;
-}
-}
+    if(Grab){//ф-ция притяжения
+        if(hit.rigidbody){
+            hit.rigidbody.velocity = (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass))*grabPower;
+        }
+    }
 
-if(Throw){//ф-ция толчка
-if(hit.rigidbody){
-hit.rigidbody.velocity = transform.forward * throwPower;
-Throw = false;
-}
-}
-}
+    if(Throw){//ф-ция толчка
+        if(hit.rigidbody){
+            hit.rigidbody.velocity = transform.forward * throwPower;
+            Throw = false;
+        }
+    }
+        /*if(Input.GetKeyDown("e") && enter){
+            //open = !open;
+        }
+        }
+        function OnGUI(){
+            if(enter){
+                GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'E' to open the door");
+            }
+        }
+        
+            //Activate the Main function when player is near the door
+        function OnTriggerEnter (other : Collider){
+            if (other.gameObject.tag == "Player") {
+                enter = true;
+            }
+        }
+        
+            //Deactivate the Main function when player is go away from door
+            function OnTriggerExit (other : Collider){
+                if (other.gameObject.tag == "Player") {
+                    enter = false;
+                }
+            }*/
+    }
